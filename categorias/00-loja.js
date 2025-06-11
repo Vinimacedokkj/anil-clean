758eac70461e817de68b79acae401bd7f0ec44e7
Vinicius <macedovinicius17@gmail.com>
2025-06-11 19:16:01 -0300

    teste para puxar apenas os containers de cada produto

--- categorias/00-loja.js
+++ categorias/00-loja.js
@@ -1,5 +1,11 @@
-fetch('00-loja.html')
+fetch('/categorias/00-loja.html')
     .then(res => res.text())
     .then(data => {
-    document.getElementById('loja').innerHTML = data;
+        document.getElementById('loja').innerHTML = data;
 });
+
+fetch('/categorias/container-de-cada-produto/triciclo-europa(vermelho).html')
+    .then(res => res.text())
+    .then(data => {
+        document.getElementById('triciclo-europa-vermelho').innerHTML = data;
+    })
