59d1b04fc9a3dd96190d0a91c9720a0d3790823c
Vinicius <macedovinicius17@gmail.com>
2025-08-18 23:42:22 -0300

    1º teste para ver se vai funcionar para adicionar os produtos na loja

--- automatização python/criar-produto-na-loja.py
+++ automatização python/criar-produto-na-loja.py
@@ -24,7 +24,11 @@ criar_produto = int(input("Quantos produtos você quer criar? "))
 
 time.sleep(5)
 
+pa.PAUSE = 1
+
 for i in range(criar_produto):
+    # Para o código funcionar, deixar aberto na aba da loja.html, no espaço em branco onde vai ser inserido o produto
+    # E na planilha, deixar em cima da célula com o nome da pasta.
     pa.hotkey("win", "4")
 
     pa.hotkey("ctrl", "a")
@@ -47,8 +51,104 @@ for i in range(criar_produto):
 
     pa.keyUp("shift")
 
-    
+    pa.write("/")
+
+    pa.hotkey("win", "2")
+
+    pa.press("f2")
+
+    pa.hotkey("ctrl", "a")
+
+    pa.hotkey("ctrl", "c")
+
+    pa.press("esc")
+
+    # Aqui vai deixar em cima da célula que possui o código em html do produto
+    pa.press("left", presses=3)
+
+    pa.hotkey("win", "9")
+
+    pa.hotkey("ctrl", "v")
 
     pa.write("/")
 
+    # Vai copiar o código em html do produto
+    pa.hotkey("win", "2")
 
+    pa.press("f2")
+
+    pa.hotkey("ctrl", "a")
+
+    pa.hotkey("ctrl", "c")
+
+    pa.press("esc")
+
+    pa.press("left")
+
+    pa.hotkey("win", "9")
+
+    pa.hotkey("ctrl", "v")
+
+    pa.press("down")
+
+    pa.press("home")
+
+    pa.press("right", presses=10)
+
+    pa.keyDown("shift")
+
+    pa.press("right", presses=24)
+    
+    pa.keyUp("shift")
+
+    pa.press("backspace")
+
+    pa.write("/img/imagens-dos-produtos/todos-os-produtos/")
+
+    pa.hotkey("win", "2")
+
+    pa.press("f2")
+
+    pa.hotkey("ctrl", "a")
+
+    pa.hotkey("ctrl", "c")
+
+    pa.press("esc")
+
+    pa.press("right", presses=4)
+
+    pa.hotkey("win", "9")
+
+    pa.hotkey("ctrl", "v")
+
+    pa.keyDown("shift")
+
+    pa.press("home")
+
+    pa.press("right", presses=10)
+    
+    pa.keyUp("shift")
+
+    pa.hotkey("ctrl", "c")
+
+    pa.press("down", presses=6)
+
+    pa.press("end")
+
+    pa.press("left", presses=32)
+
+    pa.keyDown("shift")
+
+    pa.press("left", presses=24)
+
+    pa.keyUp("shift")
+
+    pa.hotkey("ctrl", "v")
+
+    pa.press("down", presses=3)
+
+    pa.press("enter", presses=2)
+
+    pa.press("enter")
+
+    pa.press("tab")
