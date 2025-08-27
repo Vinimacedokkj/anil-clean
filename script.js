bc1c350d0958dd14ac1492e6852e89849fd240ce
Vinicius Macedo <macedovinicius17@gmail.com>
2025-08-27 15:25:00 -0300

    adição do produto em destaque

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.getElementById("whatsapp-image").addEventListener("click", function () {
+// ****************** COLETE INFANTIL PERSONALIZADO ******************
+document.querySelector("#colete-infantil-personalizado").addEventListener("click", function () {
+    // Format: https://wa.me/PHONE?text=MESSAGE
+    const phoneNumber = "5511930145556"; // Replace with your number (country code + area code + number)
+    const message = "Olá, vim através do site e gostaria de solicitar um orçamento de um colete infantil personalizado";
+
+    // Encode message for URL
+    const encodedMessage = encodeURIComponent(message);
+
+    // Open WhatsApp in a new tab
+    const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
+    window.open(whatsappUrl, "_blank");
+});
+
+// ****************** WHATSAPP REQ ORÇAMENTO ******************
+document.querySelector(".whatsapp-req-orcamento").addEventListener("click", function () {
     // Format: https://wa.me/PHONE?text=MESSAGE
     const phoneNumber = "5511930145556"; // Replace with your number (country code + area code + number)
     const message = "Olá, vim através do site e gostaria de solicitar um orçamento de um produto";
@@ -9,5 +24,4 @@ document.getElementById("whatsapp-image").addEventListener("click", function () 
     // Open WhatsApp in a new tab
     const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
     window.open(whatsappUrl, "_blank");
-    });
-
+});
