e56f828f6e46c7d9e363cac33abeb4f5b0272d28
Vinicius <macedovinicius17@gmail.com>
2025-09-04 10:59:19 -0300

    conserto dos detalhes pedidos

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ****************** COLETE INFANTIL PERSONALIZADO ******************
 document.querySelector("#colete-infantil-personalizado").addEventListener("click", function () {
     // Format: https://wa.me/PHONE?text=MESSAGE
-    const phoneNumber = "5511930145556"; // Replace with your number (country code + area code + number)
+    const phoneNumber = "551126429801"; // Replace with your number (country code + area code + number)
     const message = "Olá, vim através do site e gostaria de solicitar um orçamento de um colete infantil personalizado";
 
     // Encode message for URL
@@ -15,7 +15,7 @@ document.querySelector("#colete-infantil-personalizado").addEventListener("click
 // ****************** WHATSAPP REQ ORÇAMENTO ******************
 document.querySelector(".whatsapp-req-orcamento").addEventListener("click", function () {
     // Format: https://wa.me/PHONE?text=MESSAGE
-    const phoneNumber = "5511930145556"; // Replace with your number (country code + area code + number)
+    const phoneNumber = "551126429801"; // Replace with your number (country code + area code + number)
     const message = "Olá, vim através do site e gostaria de solicitar um orçamento de um produto";
 
     // Encode message for URL
